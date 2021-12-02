db262c463e41a8641eea05dd93ea68987837519a
Foysal <saimumislam27@gmail.com>
2021-12-02 23:02:32 +0600

    Updates

---
+++ precache-manifest.e34fcabad2261ca29016a20e01dbfb6b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c5baf02af284beacaca5b1d8390e99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a3d21af1f30ebec0fcf",
+    "url": "/static/css/main.74efa2ff.chunk.css"
+  },
+  {
+    "revision": "f110b85c4c4e0cca8262",
+    "url": "/static/js/2.b0f339ea.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.b0f339ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a3d21af1f30ebec0fcf",
+    "url": "/static/js/main.22c6697e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "1ace239bad07926ea337932a07041abe",
+    "url": "/static/media/back.1ace239b.jpg"
+  },
+  {
+    "revision": "d8a5e8f8c34ebf77a5298836118c618a",
+    "url": "/static/media/cover.d8a5e8f8.jpg"
+  },
+  {
+    "revision": "18090204a1743722d678b2026f1e404c",
+    "url": "/static/media/sample.18090204.jpg"
+  }
+]);
